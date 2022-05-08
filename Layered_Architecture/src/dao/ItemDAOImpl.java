ae303169aeb212eae695af2fa81e31b2d8be2a94
Lasari22 <lasariwijayardhana@gmail.com>
2022-05-08 18:34:32 +0530

    Refactor Generate NewId

--- Layered_Architecture/src/dao/ItemDAOImpl.java
+++ Layered_Architecture/src/dao/ItemDAOImpl.java
@@ -18,12 +18,12 @@ public class ItemDAOImpl {
         Statement stm = connection.createStatement();
         ResultSet rst = stm.executeQuery("SELECT * FROM Item");
         ArrayList<ItemDTO> allItems = new ArrayList<>();
-        while (rst.next()){
+        while (rst.next()) {
             String code = rst.getString(1);
             String description = rst.getString(2);
             BigDecimal unitPrice = rst.getBigDecimal(3);
             int qtyOnHand = rst.getInt(4);
-            allItems.add(new ItemDTO(code,description,unitPrice,qtyOnHand));
+            allItems.add(new ItemDTO(code, description, unitPrice, qtyOnHand));
         }
         return allItems;
     }
@@ -32,13 +32,13 @@ public class ItemDAOImpl {
         Connection connection = DBConnection.getDbConnection().getConnection();
         PreparedStatement pstm = connection.prepareStatement("DELETE FROM Item WHERE code=?");
         pstm.setString(1, code);
-        return pstm.executeUpdate()>0;
+        return pstm.executeUpdate() > 0;
     }
 
     public void saveItem(ItemDTO dto) throws SQLException, ClassNotFoundException {
         Connection connection = DBConnection.getDbConnection().getConnection();
         PreparedStatement pstm = connection.prepareStatement("INSERT INTO Item (code, description, unitPrice, qtyOnHand) VALUES (?,?,?,?)");
-        pstm.setString(1,dto.getCode());
+        pstm.setString(1, dto.getCode());
         pstm.setString(2, dto.getDescription());
         pstm.setBigDecimal(3, dto.getUnitPrice());
         pstm.setInt(4, dto.getQtyOnHand());
@@ -52,7 +52,7 @@ public class ItemDAOImpl {
         pstm.setBigDecimal(2, dto.getUnitPrice());
         pstm.setInt(3, dto.getQtyOnHand());
         pstm.setString(4, dto.getCode());
-        return pstm.executeUpdate()>0;
+        return pstm.executeUpdate() > 0;
     }
 
     public boolean existItem(String code) throws SQLException, ClassNotFoundException {
@@ -62,4 +62,16 @@ public class ItemDAOImpl {
         return pstm.executeQuery().next();
     }
 
+    public String generateNewId() throws SQLException, ClassNotFoundException {
+        Connection connection = DBConnection.getDbConnection().getConnection();
+        ResultSet rst = connection.createStatement().executeQuery("SELECT code FROM Item ORDER BY code DESC LIMIT 1;");
+        if (rst.next()) {
+            String id = rst.getString("code");
+            int newItemId = Integer.parseInt(id.replace("I00-", "")) + 1;
+            return String.format("I00-%03d", newItemId);
+        } else {
+            return "I00-001";
+        }
+
+    }
 }
